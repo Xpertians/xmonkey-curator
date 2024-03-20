3dd9064a4d71323057591e88e7d2361213b76267
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2024-03-20 15:05:51 -0700

    fixing logs

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 setup.py sdist bdist_wheel > logs/build.log
-pip install dist/xmonkey_curator-0.1.0-py3-none-any.whl --force-reinstall > logs/install.log
+python3 setup.py sdist bdist_wheel > scripts/build.log
+pip install dist/xmonkey_curator-0.1.0-py3-none-any.whl --force-reinstall > scripts/install.log
