2cdb72357871fd69ba85a3968c4526fac71f63ad
Oscar V <oscar.valenzuela.b@gmail.com>
2024-03-27 16:07:24 -0700

    fixing subversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='xmonkey-curator',
-    version='0.1.1',
+    version='0.1.2',
     description="Automated OSS curation scanner",
     long_description=long_description,
     long_description_content_type='text/x-rst',
