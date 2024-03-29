89c3576d52dba5a6e53593c4f7e1e373dd2d8fa3
Oscar V <oscar.valenzuela.b@gmail.com>
2024-03-29 15:26:36 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='xmonkey-curator',
-    version='0.1.2',
+    version='0.1.3',
     description="Automated OSS curation scanner",
     long_description=long_description,
     long_description_content_type='text/x-rst',
