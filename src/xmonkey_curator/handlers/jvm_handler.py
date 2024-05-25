13d5a48edec08e6f22ec0cf6caa4f352bffaa16e
Oscar V <oscar.valenzuela.b@gmail.com>
2024-05-25 11:16:20 -0700

    fixing pep8

--- src/xmonkey_curator/handlers/jvm_handler.py
+++ src/xmonkey_curator/handlers/jvm_handler.py
@@ -15,57 +15,56 @@ import re
 
 class JvmFileHandler(BaseFileHandler):
     class Constants(Enum):
-        CONSTANT_Class              = 7
-        CONSTANT_Fieldref           = 9
-        CONSTANT_Methodref          = 10
+        CONSTANT_Class = 7
+        CONSTANT_Fieldref = 9
+        CONSTANT_Methodref = 10
         CONSTANT_InterfaceMethodref = 11
-        CONSTANT_String             = 8
-        CONSTANT_Integer            = 3
-        CONSTANT_Float              = 4
-        CONSTANT_Long               = 5
-        CONSTANT_Double             = 6
-        CONSTANT_NameAndType        = 12
-        CONSTANT_Utf8               = 1
-        CONSTANT_MethodHandle       = 15
-        CONSTANT_MethodType         = 16
-        CONSTANT_InvokeDynamic      = 18
-
+        CONSTANT_String = 8
+        CONSTANT_Integer = 3
+        CONSTANT_Float = 4
+        CONSTANT_Long = 5
+        CONSTANT_Double = 6
+        CONSTANT_NameAndType = 12
+        CONSTANT_Utf8 = 1
+        CONSTANT_MethodHandle = 15
+        CONSTANT_MethodType = 16
+        CONSTANT_InvokeDynamic = 18
 
     ACCESS_FLAGS = {
         "class":  [
-            ("ACC_PUBLIC"       ,0x0001),
-            ("ACC_FINAL"        ,0x0010),
-            ("ACC_SUPER"        ,0x0020),
-            ("ACC_INTERFACE"    ,0x0200),
-            ("ACC_ABSTRACT"     ,0x0400),
-            ("ACC_SYNTHETIC"    ,0x1000),
-            ("ACC_ANNOTATION"   ,0x2000),
-            ("ACC_ENUM"         ,0x4000),
+            ("ACC_PUBLIC", 0x0001),
+            ("ACC_FINAL", 0x0010),
+            ("ACC_SUPER", 0x0020),
+            ("ACC_INTERFACE", 0x0200),
+            ("ACC_ABSTRACT", 0x0400),
+            ("ACC_SYNTHETIC", 0x1000),
+            ("ACC_ANNOTATION", 0x2000),
+            ("ACC_ENUM", 0x4000),
         ],
         "field": [
-            ("ACC_PUBLIC"       ,0x0001),
-            ("ACC_PRIVATE"      ,0x0002),
-            ("ACC_PROTECTED"    ,0x0004),
-            ("ACC_STATIC"       ,0x0008),
-            ("ACC_FINAL"        ,0x0010),
-            ("ACC_VOLATILE"     ,0x0040),
-            ("ACC_TRANSIENT"    ,0x0080),
-            ("ACC_SYNTHETIC"    ,0x1000),
-            ("ACC_ENUM"         ,0x4000),
+            ("ACC_PUBLIC", 0x0001),
+            ("ACC_PRIVATE", 0x0002),
+            ("ACC_PROTECTED", 0x0004),
+            ("ACC_STATIC", 0x0008),
+            ("ACC_FINAL", 0x0010),
+            ("ACC_VOLATILE", 0x0040),
+            ("ACC_TRANSIENT", 0x0080),
+            ("ACC_SYNTHETIC", 0x1000),
+            ("ACC_ENUM", 0x4000),
         ],
         "method": [
-            ("ACC_PUBLIC"       ,0x0001),
-            ("ACC_PRIVATE"      ,0x0002),
-            ("ACC_PROTECTED"    ,0x0004),
-            ("ACC_STATIC"       ,0x0008),
-            ("ACC_FINAL"        ,0x0010),
-            ("ACC_SYNCHRONIZED" ,0x0020),
-            ("ACC_BRIDGE"       ,0x0040),
-            ("ACC_VARARGS"      ,0x0080),
-            ("ACC_NATIVE"       ,0x0100),
-            ("ACC_ABSTRACT"     ,0x0400),
-            ("ACC_STRICT"       ,0x0800),
-            ("ACC_SYNTHETIC"    ,0x1000),
+            ("ACC_PUBLIC", 0x0001),
+            ("ACC_PRIVATE", 0x0002),
+            ("ACC_PROTECTED", 0x0004),
+            ("ACC_STATIC", 0x0008),
+            ("ACC_FINAL", 0x0010),
+            ("ACC_SYNCHRONIZED", 0x0020),
+            ("ACC_BRIDGE", 0x0040),
+            ("ACC_VARARGS", 0x0080),
+            ("ACC_NATIVE", 0x0100),
+            ("ACC_ABSTRACT", 0x0400),
+            ("ACC_STRICT", 0x0800),
+            ("ACC_SYNTHETIC", 0x1000),
         ],
     }
 
@@ -83,7 +82,6 @@ class JvmFileHandler(BaseFileHandler):
         base_name = os.path.basename(self.file_path)
         file_name = os.path.splitext(base_name)[0]
         symbols.append(file_name.lower())
-        
         unique_strings = set()
         with open(self.file_path, mode="rb") as f:
             class_file = self.parse_class_file(BytesIO(f.read()))
@@ -103,18 +101,14 @@ class JvmFileHandler(BaseFileHandler):
     def parse_ux(self, file: BytesIO, length: int) -> int:
         return int.from_bytes(file.read(length), "big")
 
-
     def parse_u1(self, file: BytesIO) -> int:
         return self.parse_ux(file, 1)
-
 
     def parse_u2(self, file: BytesIO) -> int:
         return self.parse_ux(file, 2)
 
-
     def parse_u4(self, file: BytesIO) -> int:
         return self.parse_ux(file, 4)
-
 
     def parse_constant_pool(self, f: BytesIO, pool_size: int) -> int:
         constant_pool = []
@@ -131,7 +125,9 @@ class JvmFileHandler(BaseFileHandler):
                 cp_info["tag"] = constant.value
                 cp_info["class_index"] = self.parse_u2(f)
                 cp_info["name_and_type_index"] = self.parse_u2(f)
-            elif constant in (self.Constants.CONSTANT_Class, self.Constants.CONSTANT_String):
+            elif constant in (
+                self.Constants.CONSTANT_Class, self.Constants.CONSTANT_String
+            ):
                 cp_info["tag"] = constant.value
                 cp_info["name_index"] = self.parse_u2(f)
             elif constant == self.Constants.CONSTANT_Utf8:
@@ -142,10 +138,14 @@ class JvmFileHandler(BaseFileHandler):
                 cp_info["tag"] = constant.value
                 cp_info["name_index"] = self.parse_u2(f)
                 cp_info["descriptor_index"] = self.parse_u2(f)
-            elif constant in (self.Constants.CONSTANT_Integer, self.Constants.CONSTANT_Float):
+            elif constant in (
+                self.Constants.CONSTANT_Integer, self.Constants.CONSTANT_Float
+            ):
                 cp_info["tag"] = constant.value
                 cp_info["bytes"] = f.read(4)
-            elif constant in (self.Constants.CONSTANT_Long, self.Constants.CONSTANT_Double):
+            elif constant in (
+                self.Constants.CONSTANT_Long, self.Constants.CONSTANT_Double
+            ):
                 cp_info["tag"] = constant.value
                 cp_info["high_bytes"] = f.read(4)
                 cp_info["low_bytes"] = f.read(4)
@@ -161,31 +161,25 @@ class JvmFileHandler(BaseFileHandler):
                 cp_info["bootstrap_method_attr_index"] = self.parse_u2(f)
                 cp_info["name_and_type_index"] = self.parse_u2(f)
             else:
-                assert False, f"Unexpected tag encountered {tag = }"
+                assert False, f"Unexpected tag encountered {tag=}"
             constant_pool.append(cp_info)
         return constant_pool
-
 
     def parse_access_flags(self, val: int, flags: [(str, int)]) -> list[str]:
         return [name for (name, mask) in flags if not (val & mask)]
 
-
     def parse_attributes(self, f: BytesIO, attributes_count: int) -> list:
         attributes = []
-
         for _ in range(attributes_count):
             attribute_info = {}
             attribute_info["attribute_name_index"] = self.parse_u2(f)
             attribute_info["attribute_length"] = self.parse_u4(f)
             attribute_info["info"] = f.read(attribute_info["attribute_length"])
             attributes.append(attribute_info)
-
         return attributes
-
 
     def parse_methods(self, f: BytesIO, methods_count: int) -> list:
         methods = []
-
         for _ in range(methods_count):
             method_info = {}
             method_info["access_flags"] = self.parse_access_flags(
@@ -194,14 +188,14 @@ class JvmFileHandler(BaseFileHandler):
             method_info["name_index"] = self.parse_u2(f)
             method_info["descriptor_index"] = self.parse_u2(f)
             method_info["attributes_count"] = self.parse_u2(f)
-            method_info["attributes"] = self.parse_attributes(f, method_info["attributes_count"])
+            method_info["attributes"] = self.parse_attributes(
+                f, method_info["attributes_count"]
+            )
             methods.append(method_info)
         return methods
 
-
     def parse_fields(self, f: BytesIO, fields_count: int) -> dict:
         fields = []
-
         for _ in range(fields_count):
             field_info = {}
             field_info["access_flags"] = self.parse_access_flags(
@@ -210,20 +204,22 @@ class JvmFileHandler(BaseFileHandler):
             field_info["name_index"] = self.parse_u2(f)
             field_info["descriptor_index"] = self.parse_u2(f)
             field_info["attributes_count"] = self.parse_u2(f)
-            field_info["attributes"] = self.parse_attributes(f, field_info["attributes_count"])
+            field_info["attributes"] = self.parse_attributes(
+                f, field_info["attributes_count"]
+            )
             fields.append(field_info)
-
         return fields
-
 
     def parse_interfaces(self, f: BytesIO, interfaces_count: int) -> dict:
         interfaces = []
         for _ in range(interfaces_count):
             self.parse_u1(f)  # Discard tag
-            class_info = {"tag": "CONSTANT_Class", "name_index": self.parse_u2(f)}
+            class_info = {
+                "tag": "CONSTANT_Class",
+                "name_index": self.parse_u2(f)
+            }
             interfaces.append(class_info)
         return interfaces
-
 
     def parse_class_file(self, f: BytesIO) -> dict:
         class_file = {}
@@ -234,16 +230,23 @@ class JvmFileHandler(BaseFileHandler):
         class_file["constant_pool"] = self.parse_constant_pool(
             f, class_file["constant_pool_count"] - 1
         )
-        class_file["access_flags"] = self.parse_access_flags(self.parse_u2(f), self.ACCESS_FLAGS["class"])
+        class_file["access_flags"] = self.parse_access_flags(
+            self.parse_u2(f), self.ACCESS_FLAGS["class"]
+        )
         class_file["this_class"] = self.parse_u2(f)
         class_file["super_class"] = self.parse_u2(f)
         class_file["interfaces_count"] = self.parse_u2(f)
-        class_file["interfaces"] = self.parse_interfaces(f, class_file["interfaces_count"])
+        class_file["interfaces"] = self.parse_interfaces(
+            f, class_file["interfaces_count"]
+        )
         class_file["fields_count"] = self.parse_u2(f)
         class_file["fields"] = self.parse_fields(f, class_file["fields_count"])
         class_file["methods_count"] = self.parse_u2(f)
-        class_file["methods"] = self.parse_methods(f, class_file["methods_count"])
+        class_file["methods"] = self.parse_methods(
+            f, class_file["methods_count"]
+        )
         class_file["attributes_count"] = self.parse_u2(f)
-        class_file["attributes"] = self.parse_attributes(f, class_file["attributes_count"])
-
+        class_file["attributes"] = self.parse_attributes(
+            f, class_file["attributes_count"]
+        )
         return class_file
